01c72bc2ae366260c0d046371a31d7ff186b3110
EsmeeZhang <esmezh42@gmail.com>
2020-07-20 10:48:50 -0400

    game

--- .idea/DOMPrac/DOMPra.js
+++ .idea/DOMPrac/DOMPra.js
@@ -10,5 +10,30 @@ dice = Math.floor(Math.random() * 6) +1;
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + "</em>";
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 var x = document.querySelector('#score-0').textContent;
 document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    // 1.random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. update the round score if the rolled number was not a 1
+    if(dice !== 1){
+        roundScore += dice;
+        documnet.querySelector('current-' + activePlayer).textContent = roundScore;
+
+    }else{
+        activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+    }
+
+});

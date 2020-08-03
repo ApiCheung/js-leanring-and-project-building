4632902f034d1742a8ec088b91aad8d63c5710af
EsmeeZhang <esmezh42@gmail.com>
2020-08-03 16:17:55 -0400

    game

--- src/ES6/ClassesES6.js
+++ src/ES6/ClassesES6.js
@@ -13,6 +13,24 @@ Person5.prototype.calculateAge =
 
     var john5 = new Person5('John', 1990, 'teacher');
 
+var Athlete5 = function(name, yearOfBirth, job, olmicGames, medals){
+    Person5.call(this, name, yearOfBirth,job);
+    this.olmicGames = olmicGames;
+    this.medals = medals;
+}
+//make inheritance work
+Athlete5.prototype = Object.create(Person5.prototype);
+
+
+Athlete5.prototype.wonMedal = function(){
+    this.medals ++;
+    console.log(this.medals);
+}
+
+
+var johnAthlete5 = new Athelete5('John', 1990, 'swimmer',3,10);
+
+johnAthlete5.calculateAge();
 //es6
 
 class Person6{
@@ -29,12 +47,28 @@ class Person6{
     }
     //static not inherited
 
-    static greeting(){
-        console.log('hey there!');
+    // static greeting(){
+    //     console.log('hey there!');
+    //}
+
+}
+
+class Athelete6 extends Person6{
+    constructor(name, yearOfBirth, job, olymicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olymucGames = olymicGames;
+        this.medals = medals;
     }
 
+    wonMedal(){
+        this.medal ++;
+    }
 }
 
 const john6 = new Person6('John', 1990, 'teacher');
 
 Person6.greeting();
+
+const johnAthlete6 = new Athelete6('john',1990, 'swimmer',3,10);
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
